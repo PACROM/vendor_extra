3993a2b84441e8395823147974c2152d26649028
ShoreDroid <53381903+PACROM@users.noreply.github.com>
2019-08-04 09:05:28 -0400

    just on the backburner for now

--- product.mk
+++ product.mk
@@ -1,2 +1,2 @@
-PRODUCT_BUILD_PROP_OVERRIDES += \
-ro.build.display.id=PQ3A.190705.003-pacrom.$(shell date -u +%Y%m%d)-test-keys
+#PRODUCT_BUILD_PROP_OVERRIDES += \
+#ro.build.display.id=PQ3A.190705.003-pacrom.$(shell date -u +%Y%m%d)-test-keys
